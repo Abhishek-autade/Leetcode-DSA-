4883e6b462769a3294633d21192a706f261c0909
Abhishek-autade <abhishekautade09@gmail.com>
2026-09-15 19:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (38.38%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    // Sort
+    public ListNode mergesort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Find middle
+        ListNode mid = getMiddle(head);
+        ListNode rightHead = mid.next;
+        mid.next = null; // split list
+
+        ListNode leftSorted = mergesort(head);
+        ListNode rightSorted = mergesort(rightHead);
+
+        // Merge 
+        return mergeTwoLists(leftSorted, rightSorted);
+    }
+    private ListNode getMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
